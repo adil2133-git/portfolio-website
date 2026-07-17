6fc7d8a52f1a35e03a98db78efb347b8a46aa393
adil2133-git <adilaadi340@gmail.com>
2026-07-17 16:08:21 +0530

    githubActivity component created

---
+++ components/GithubActivity.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import GitHubCalendar from "react-github-calendar";
+
+export default function GithubActivity() {
+  return (
+    <GitHubCalendar
+      username="adil2133-git"
+      colorScheme="dark"
+      theme={{
+        dark: ["#12161F", "#0F3D38", "#1D6B5F", "#2A9D8A", "#3FC7B8"],
+      }}
+      fontSize={12}
+      blockSize={11}
+      blockMargin={4}
+      hideColorLegend={false}
+      hideMonthLabels={false}
+    />
+  );
+}
